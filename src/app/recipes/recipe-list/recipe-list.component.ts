db4fc6a83eaabffa6db398a1016bb4f48bf760ca
DelMouse <del.groh@icloud.com>
2022-05-11 20:50:37 -0500

    56

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-      new Recipe('A test recipe','This is a test', 'https://rb.gy/gc3yzo')
+      new Recipe('A test recipe','This is a test', 'https://www.pngitem.com/pimgs/m/235-2354731_recipes-png-transparent-png.png')
   ];
 
   constructor() { }
